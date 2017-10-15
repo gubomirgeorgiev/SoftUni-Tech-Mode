dec84c760a3320c1a6479bdbd4721afa1d55601e
Lyubomir Georgiev <gubomirgeorgiev@gmail.com>
2017-10-15 14:46:33 +0300

    homework update C# list lab

--- 07.Lists_Lab_CSharp/P07CountNumbers/Program.cs
+++ 07.Lists_Lab_CSharp/P07CountNumbers/Program.cs
@@ -10,6 +10,22 @@ namespace P07CountNumbers
     {
         static void Main(string[] args)
         {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            input.Sort((a, b) => a.CompareTo(b));
+            var dictionary = new Dictionary<int, int>();
+            int[] numsZeroToTousen = new int[1001];
+            foreach (int num in input)
+            {
+                numsZeroToTousen[num]++;
+            }
+            for (int i = 0; i < numsZeroToTousen.Length; i++)
+            {
+                if (numsZeroToTousen[i] > 0)
+                {
+                    dictionary.Add(i, numsZeroToTousen[i]);
+                }
+            }
+            Console.WriteLine(string.Join("", dictionary.Select(kvp => kvp.Key + " -> " + kvp.Value.ToString() + "\n")));
         }
     }
 }
---
+++ 07.Lists_Lab_CSharp/P07CountNumbers_UsingArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07CountNumbers_UsingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int max = input.Max();
+            var counts = new int[max + 1];
+            for (int i=0; i< input.Length; i++)
+            {
+                    counts[input[i]]++;
+            }
+            for(int i =0;i< counts.Length; i++)
+            {
+                if (counts[i] > 0)
+                {
+                    Console.WriteLine(i + " -> " + string.Join(" ", counts[i]));
+                }
+            }
+        }
+    }
+}
